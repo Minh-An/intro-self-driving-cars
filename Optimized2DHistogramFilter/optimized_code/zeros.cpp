599a8cc1078e274f8e03d11d0fcfea56b703bd26
An Doan <minh.an.doan@gmail.com>
2019-06-23 08:39:47 -0700

    Modify initialize_belief function for 2D Histogram Filter.

--- Optimized2DHistogramFilter/optimized_code/zeros.cpp
+++ Optimized2DHistogramFilter/optimized_code/zeros.cpp
@@ -3,9 +3,8 @@
 using namespace std;
 
 vector < vector <float> > zeros(int height, int width) {  
-    // OPTIMIZATION: Reserve space in memory for vectors
-    vector <float> newRow(width, 0);
-    vector < vector <float> > newGrid(height, newRow);
-
-    return newGrid;
+	// OPTIMIZATION: Reserve space in memory for vectors
+	vector <float> newRow(width, 0);
+	vector < vector <float> > newGrid(height, newRow);
+	return newGrid;
 }
