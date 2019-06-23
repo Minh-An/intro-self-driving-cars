8be4474c28c244d86849244a9c494e325cad8341
An Doan <minh.an.doan@gmail.com>
2019-06-23 10:58:46 -0700

    Optimize normalize function for 2D Histogram Filter.

--- Optimized2DHistogramFilter/optimized_code/normalize.cpp
+++ Optimized2DHistogramFilter/optimized_code/normalize.cpp
@@ -1,38 +1,22 @@
+#include <numeric>
 #include "headers/normalize.h"
+
 using namespace std;
 
-// OPTIMIZATION: Pass variable by reference
-vector< vector<float> > normalize(vector< vector <float> > grid) {
+vector< vector<float> > normalize(vector< vector <float> > & grid) {
 
-  	// OPTIMIZATION: Avoid declaring and defining 				// intermediate variables that are not needed.
-	float total = 0.0;
-	int i;
-	int j;
-	vector<float> row;
-	vector<float> newRow;
-	float oldProb;
-	for (i = 0; i < grid.size(); i++)
+	float sum = 0;
+	for (int i = 0; i < grid.size(); i++)
 	{
-		row = grid[i];
-		for (j=0; j< row.size(); j++)
+		sum = accumulate(grid[i].begin(), grid[i].end(), sum);
+	}
+	for (int i = 0; i < grid.size(); i++)
+	{
+		for (int j = 0; j < grid[i].size(); j++)
 		{
-			oldProb = row[j];
-			total += oldProb;
+			grid[i][j] = grid[i][j] / sum;
 		}
 	}
 
-	vector< vector<float> > newGrid;
-
-	for (i = 0; i < grid.size(); i++) {
-		vector<float> row = grid[i];
-		newRow.clear();
-		for (j=0; j< row.size(); j++) {
-			float oldProb = row[j];
-			float newProb = oldProb / total;
-			newRow.push_back(newProb);
-		}
-		newGrid.push_back(newRow);
-	}
-
-	return newGrid;
+	return grid;
 }
