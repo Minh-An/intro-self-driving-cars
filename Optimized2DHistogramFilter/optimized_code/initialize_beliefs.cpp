cf0378098cc2ec98add9637f7a100a530b1e86fd
An Doan <minh.an.doan@gmail.com>
2019-06-23 10:57:37 -0700

    Optimize initialize_beliefs function to pass by reference for 2D Histogram Filter.

--- Optimized2DHistogramFilter/optimized_code/initialize_beliefs.cpp
+++ Optimized2DHistogramFilter/optimized_code/initialize_beliefs.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 // OPTIMIZATION: pass large variables by reference
-vector< vector <float> > initialize_beliefs(vector< vector <char> > grid) {
+vector< vector <float> > initialize_beliefs(vector< vector <char> >& grid) {
   	float prob_per_cell = 1.0 / ( (float) grid.size() * grid[0].size()) ;
 	vector<float> newRow(grid[0].size(), prob_per_cell);
 	vector< vector <float> > newGrid(grid.size(), newRow);
