7c4d5518237da5506952aa262090c3d32c605760
An Doan <minh.an.doan@gmail.com>
2019-06-23 08:31:20 -0700

    Optimize zeros.cpp for 2D Histogram Filter.

--- Optimized2DHistogramFilter/optimized_code/zeros.cpp
+++ Optimized2DHistogramFilter/optimized_code/zeros.cpp
@@ -2,21 +2,10 @@
 
 using namespace std;
 
-vector < vector <float> > zeros(int height, int width) {
-	int i, j;
-  
-	// OPTIMIZATION: Reserve space in memory for vectors
-	vector < vector <float> > newGrid;
-	vector <float> newRow;
+vector < vector <float> > zeros(int height, int width) {  
+    // OPTIMIZATION: Reserve space in memory for vectors
+    vector <float> newRow(width, 0);
+    vector < vector <float> > newGrid(height, newRow);
 
-  	// OPTIMIZATION: nested for loop not needed
-    // because every row in the matrix is exactly the same
-	for (i=0; i<height; i++) {
-		newRow.clear();
-		for (j=0; j<width; j++) {
-			newRow.push_back(0.0);
-		}
-		newGrid.push_back(newRow);
-	}
-	return newGrid;
+    return newGrid;
 }
