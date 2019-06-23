e3631bff6ea3aaab33c1dd9c7d0b1f285b7e769b
An Doan <minh.an.doan@gmail.com>
2019-06-23 10:58:07 -0700

    Optimize blur function for 2D Histogram Filter

--- Optimized2DHistogramFilter/optimized_code/blur.cpp
+++ Optimized2DHistogramFilter/optimized_code/blur.cpp
@@ -1,127 +1,38 @@
 #include "headers/blur.h"
 
+
 using namespace std;
 
-vector < vector <float> > blur(vector < vector < float> > grid, float blurring) {
+vector < vector <float> > blur(vector < vector < float> >& grid, float blurring) {
 
-	// initialize variables
-	vector < vector <float> > window;
-	vector < vector <float> > newGrid;
-	vector <float> row;
-	vector <float> newRow;
+	static float center = 1.0 - blurring;
+	static float corner = blurring / 12.0;
+	static float adjacent = blurring / 6.0;
 
-	int height;
-	int width;
-	float center, corner, adjacent;
+	static vector<vector<float>> window = {
+		{corner, adjacent, corner},
+		{adjacent, center, adjacent},
+		{corner, adjacent, corner}};
 
-	height = grid.size();
-	width = grid[0].size();
+	int height = grid.size();
+	int width = grid[0].size();
 
-	// calculate blur factors
-	center = 1.0 - blurring;
-	corner = blurring / 12.0;
-	adjacent = blurring / 6.0;
+	vector<float> row(width, 0);
+	vector<vector<float>> newGrid(height, row);
 
-	int i, j;
-	float val;
-
-	// 2D vector reprenting the blur filter
-	for (i=0; i<3; i++) {
-		row.clear();
-		for (j=0; j<3; j++) {
-			switch (i) {
-				case 0: 
-				switch (j) {
-					case 0: 
-					val = corner;
-					break;
-
-					case 1: 
-					val = adjacent;
-					break;
-
-					case 2: 
-					val = corner;
-					break;
-				}
-				break; 
-
-				case 1:
-				switch (j) {
-					case 0: 
-					val = adjacent;
-					break;
-
-					case 1: 
-					val = center;
-					break;
-					
-					case 2: 
-					val = adjacent;
-					break;
-				}
-				break;
-
-				case 2:
-				switch(j) {
-					case 0: 
-					val = corner;
-					break;
-
-					case 1: 
-					val = adjacent;
-					break;
-					
-					case 2: 
-					val = corner;
-					break;
-				}
-				break;
-			}
-			row.push_back(val);
-		}
-		window.push_back(row);
-	}
-
-
-	// variables for blur calculations
-	vector <int> DX;
-	vector <int> DY;
-
-	DX.push_back(-1); DX.push_back(0); DX.push_back(1);
-	DY.push_back(-1); DY.push_back(0); DY.push_back(1);
-
-	int dx;
-	int dy;
-	int ii;
-	int jj;
-	int new_i;
-	int new_j;
-	float multiplier;
-	float newVal;
-
-	// initialize new grid to zeros
-	for (i=0; i<height; i++) {
-		newRow.clear();
-		for (j=0; j<width; j++) {
-			newRow.push_back(0.0);
-		}
-		newGrid.push_back(newRow);
-	}
-
-	// blur the grid and store in a new 2D vector
-	for (i=0; i< height; i++ ) {
-		for (j=0; j<width; j++ ) {
-			val = grid[i][j];
-			newVal = val;
-			for (ii=0; ii<3; ii++) {
-				dy = DY[ii];
-				for (jj=0; jj<3; jj++) {
-					dx = DX[jj];
-					new_i = (i + dy + height) % height;
-					new_j = (j + dx + width) % width;
-					multiplier = window[ii][jj];
-					newGrid[new_i][new_j] += newVal * multiplier;
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			float grid_val = grid[i][j];
+			for (int dx = -1; dx < 2; dx++)
+			{
+				for (int dy = -1; dy < 2; dy++)
+				{
+					float coefficient = window[dx + 1][dy + 1];
+					int new_i = (((i + dy) % height) + height) % height;
+					int new_j = (((j + dx) % width) + width) % width;
+					newGrid[new_i][new_j] += coefficient * grid_val;
 				}
 			}
 		}
